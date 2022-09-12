89381749049a832916f9525f62d49177a12adcee
sergo321 <104622860+sergo321@users.noreply.github.com>
2022-09-12 21:17:30 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+// function send(){
+//   let arr=document.getElementsByClassName("count")
+//   let tot=0;
+//   for(let i=0;i<arr.length;i++){
+//       if(parseFloat(arr[i].value))
+//           tot += parseFloat(arr[i].value);
+//   }
+//   document.getElementById('answer').value = tot;
+// }
